7418b1b7eba32a23edf9a6f856fd3f73534acc41
Celorio2001 <jahir.celorio2001@gmail.com>
2023-03-15 17:40:54 -0500

    Borrando componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
