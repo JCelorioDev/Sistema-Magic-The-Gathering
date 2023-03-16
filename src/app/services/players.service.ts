ff3c74bd86f76416abb533784ff80ac9a86fd3bb
Celorio2001 <jahir.celorio2001@gmail.com>
2023-03-16 00:30:34 -0500

    Añadiendo el update

--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import {Firestore,addDoc, query, where, collectionData} from '@angular/fire/firestore';
+import {Firestore,addDoc, query, where, collectionData,getDocs,doc,updateDoc} from '@angular/fire/firestore';
 import { Player } from '../commons/interface/player.interface';
 import { collection } from '@firebase/firestore';
-import { Observable } from 'rxjs';
+import { async, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -26,4 +26,14 @@ export class PlayersService {
     return collectionData(q) as unknown as Observable<Player[]>;
   }
 
+  async updatePlayer(player: Player){
+    const playerRef = collection(this.firestore, 'players');
+    let q = query(playerRef, where('id', '==',player.id));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach( async (document) =>{
+      const docRef = doc(this.firestore, 'players',document.id);
+      await updateDoc(docRef, {...player})
+    })
+  }
 }
